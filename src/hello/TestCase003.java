072d647ab448d4d7f0f5b6ee178ead3829343a17
StefanGajic91 <95548833+StefanGajic91@users.noreply.github.com>
2022-01-24 11:53:47 -0500

    Creating conflict in Testcase003 in master

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class TestCase003 {
+    // this is unknown changes to feature branch
+    //that exists in master,
+    // now got will not know what to do with this
+    // when we merge and conflict will happen
+}
